571ce6144642e64eb6cdb703c405629939348ad9
HwangSeungHyeon <xlddy02@gmail.com>
2024-04-24 14:38:02 +0900

    Initial commit

---
+++ src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a;
+        a = 20;
+
+        for(int i=0; i<3; i++){
+            System.out.println(a);
+        }
+    }
+}
